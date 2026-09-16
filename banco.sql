7ead77378ccceeac88f59c043cf50416a43ff85a
Aluno <e023aluno@outlook.com>
2026-09-16 19:56:00 -0300

    BaseMVC Completa

--- banco.sql
+++ banco.sql
@@ -1,9 +1,59 @@
 CREATE DATABASE IF NOT EXISTS mvc;
-
 USE mvc;
 
 CREATE TABLE IF NOT EXISTS cliente (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
-    cpf VARCHAR(100) NOT NULL   
+    cpf VARCHAR(100) NOT NULL
 );
+
+-- -----------------------------------------------------
+-- Procedure: Inserir Cliente
+-- -----------------------------------------------------
+DELIMITER $$
+CREATE PROCEDURE sp_create_client(
+    IN p_nome VARCHAR(100),
+    IN p_cpf VARCHAR(100)
+)
+BEGIN
+    INSERT INTO cliente (nome, cpf) VALUES (p_nome, p_cpf);
+    -- Retorna o ID que acabou de ser gerado
+    SELECT LAST_INSERT_ID() AS insertId;
+END $$
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- Procedure: Listar Todos os Clientes
+-- -----------------------------------------------------
+DELIMITER $$
+CREATE PROCEDURE sp_get_all_clients()
+BEGIN
+    SELECT * FROM cliente;
+END $$
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- Procedure: Atualizar Cliente
+-- -----------------------------------------------------
+DELIMITER $$
+CREATE PROCEDURE sp_update_client(
+    IN p_id INT,
+    IN p_nome VARCHAR(100),
+    IN p_cpf VARCHAR(100)
+)
+BEGIN
+    UPDATE cliente SET nome = p_nome, cpf = p_cpf WHERE id = p_id;
+END $$
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- Procedure: Deletar Cliente
+-- -----------------------------------------------------
+DELIMITER $$
+CREATE PROCEDURE sp_delete_client(
+    IN p_id INT
+)
+BEGIN
+    DELETE FROM cliente WHERE id = p_id;
+END $$
+DELIMITER ;
--- models/ClienteModel.js
+++ models/ClienteModel.js
@@ -1,58 +1,60 @@
 const db = require('../config/db');
 
 const Cliente = {
-
-    createNewClient: async (nome, cpf) => {
-        try {
-            const [result] = await db.execute('INSERT INTO Cliente (nome, cpf) VALUES(?, ?)', [nome, cpf]);
-            return { id: result.insertId, nome, cpf };
-        } catch (error) {
-            throw error;
-        }
-    },
-
-    getAllClients: async () => {
-        try {
-            const [rows] = await db.query('SELECT * FROM Cliente');
-            return rows;
-        } catch (error) {
-            throw error;
-        }
-    },
-
-    // Corrigido de 'upadteClient' para 'updateClient'
-    updateClient: async (id, nome, cpf) => {
-        try {
-            if (!id) {
-                throw new Error('ID do cliente é necessário!');
-            }
-            const [result] = await db.execute('UPDATE Cliente SET nome = ?, cpf = ? WHERE id = ?', [nome, cpf, id]);
-
-            if (result.affectedRows === 0) {
-                throw new Error('Cliente não cadastrado!');
-            }
-        } catch (error) {
-            throw error;
-        }
-    },
-
-    deleteClient: async (id) => {
-        try {
-            if (!id) {
-                throw new Error('ID do cliente é necessário!');
-            }
-
-            const [result] = await db.execute('DELETE FROM Cliente WHERE id = ?', [id]);
-
-            if (result.affectedRows === 0) {
-                throw new Error('Cliente não encontrado');
-            }
-            return { message: 'Cliente excluído com sucesso' };
-
-        } catch (error) {
-            throw error;
-        }
+  createNewClient: async (nome, cpf) => {
+    try {
+      const [[procedureResult]] = await db.execute('CALL sp_create_client(?, ?)', [nome, cpf]);
+      
+      const insertId = procedureResult[0].insertId; 
+      
+      return { id: insertId, nome, cpf };
+    } catch (error) {
+      throw error;
     }
-}; // Fechamento correto do objeto Cliente
+  },
+
+  getAllClients: async () => {
+    try {
+      const [[rows]] = await db.query('CALL sp_get_all_clients()');
+      return rows;
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  updateClient: async (id, nome, cpf) => {
+    try {
+      if (!id) {
+        throw new Error('ID do cliente é necessário!');
+      }
+
+      const [result] = await db.execute('CALL sp_update_client(?, ?, ?)', [id, nome, cpf]);
+      
+      if (result.affectedRows === 0) {
+        throw new Error('Cliente não cadastrado!');
+      }
+    } catch (error) {
+      throw error;
+    }
+  },
+
+  deleteClient: async (id) => {
+    try {
+      if (!id) {
+        throw new Error('ID do cliente é necessário!');
+      }
+
+      const [result] = await db.execute('CALL sp_delete_client(?)', [id]);
+      
+      if (result.affectedRows === 0) {
+        throw new Error('Cliente não encontrado');
+      }
+
+      return { message: 'Cliente excluído com sucesso' };
+    } catch (error) {
+      throw error;
+    }
+  }
+};
 
 module.exports = Cliente;
